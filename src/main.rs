42ba32f8aaa617429a55df453dd4934c67ad59fc
Alex Mastro <amastro@fb.com>
2022-07-25 09:58:43 -0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::sync::mpsc::channel;
 use std::thread;
 use std::time::Duration;
-use std::time::Instant;
 
 use camino::Utf8Path;
 use camino::Utf8PathBuf;
@@ -27,12 +26,9 @@ struct Args {
 }
 
 fn read_svg(path: &Utf8Path) -> anyhow::Result<usvg::Tree> {
-    println!("reading {}", path);
-    let now = Instant::now();
     let svg_data = std::fs::read(path)?;
     let opt = usvg::Options::default();
     let rtree = usvg::Tree::from_data(&svg_data, &opt.to_ref())?;
-    println!("took {:?}", now.elapsed());
     Ok(rtree)
 }
 
@@ -54,31 +50,27 @@ fn main() -> anyhow::Result<()> {
     let input_path = args.input.clone();
     thread::spawn(move || {
         while let Ok(_event) = rx.recv() {
-            println!("got file changed event, notifying event loop");
             evp.send_event(()).unwrap();
         }
     });
 
-    println!("allocating buffers");
-    let now = Instant::now();
     let PhysicalSize { width, height } = window.inner_size();
     let surface_texture = SurfaceTexture::new(width, height, &window);
     let mut pixbuf = Pixels::new(width, height, surface_texture)?;
     pixbuf.set_clear_color(pixels::wgpu::Color::WHITE);
-    println!("took {:?}", now.elapsed());
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
         match event {
             Event::UserEvent(_) => {
-                rtree = read_svg(&input_path).unwrap();
-                window.request_redraw();
+                if let Ok(new) = read_svg(&input_path) {
+                    rtree = new;
+                    window.request_redraw();
+                }
             }
             Event::RedrawRequested(window_id) if window_id == window.id() => {
                 let PhysicalSize { width, height } = window.inner_size();
-                println!("redrawing at {width}x{height}");
-                let now = Instant::now();
                 pixbuf.resize_surface(width, height);
                 pixbuf.resize_buffer(width, height);
                 pixbuf.get_frame().fill(0);
@@ -92,11 +84,9 @@ fn main() -> anyhow::Result<()> {
                 );
 
                 pixbuf.render().unwrap();
-                println!("took {:?}", now.elapsed());
             }
             Event::WindowEvent { window_id, event } if window_id == window.id() => match event {
-                WindowEvent::Resized(PhysicalSize { width, height }) => {
-                    println!("resized to {width}x{height}");
+                WindowEvent::Resized(_) => {
                     window.request_redraw();
                 }
                 WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
